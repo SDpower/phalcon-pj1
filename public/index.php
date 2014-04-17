638b1b977e6be5b576ddc1355b23bb3e1f1582e5
SDpower <info@sd.idv.tw>
2014-04-17 11:47:44 +0800

    add facebook XHPROF

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+//Start enable facebook XHPROF
+xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 
 error_reporting(E_ALL);
 
@@ -29,3 +31,15 @@ try {
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
+
+//end of facebook XHPROF
+$xhprof_data = xhprof_disable();
+$XHPROF_ROOT = __DIR__ . " /../../www/";
+include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+
+$xhprof_runs = new XHProfRuns_Default();
+$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
+
+echo "http://localhost/xhprof_html/index.php?run={$run_id}&source=xhprof_testing\n";
+
